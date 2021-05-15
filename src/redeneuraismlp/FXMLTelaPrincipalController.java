96e27257476f008b7ef46d980b657a270cbfedb4
Guilherme Gomes leal <guilhermegomesleal@hotmail.com>
2021-05-15 15:28:27 -0300

    tela feita

--- src/redeneuraismlp/FXMLTelaPrincipalController.java
+++ src/redeneuraismlp/FXMLTelaPrincipalController.java
@@ -1,25 +1,64 @@
 package redeneuraismlp;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXCheckBox;
+import com.jfoenix.controls.JFXTextField;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
 
 public class FXMLTelaPrincipalController implements Initializable {
     
     @FXML
-    private Label label;
-    
+    private TableView<?> tableCsv;
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-     
-    }
+    private JFXTextField txfCamadaEntrada;
+    @FXML
+    private JFXTextField txfCamadaSaida;
+    @FXML
+    private JFXTextField txfCamadaOculta;
+    @FXML
+    private JFXTextField txfValorErro;
+    @FXML
+    private JFXTextField txfNumeroMaximo;
+    @FXML
+    private JFXTextField txfTaxaAprend;
+    @FXML
+    private JFXCheckBox ckbLinear;
+    @FXML
+    private JFXCheckBox ckbLogis;
+    @FXML
+    private JFXCheckBox ckbHiper;
+    @FXML
+    private JFXButton btnFecharArquivo;
+    @FXML
+    private JFXButton btnArquivo;
+    @FXML
+    private JFXCheckBox ckbisTeste;
+    @FXML
+    private JFXButton btnExecutar;
+    @FXML
+    private JFXCheckBox ckbisMSLINT;
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         
     }    
+
+    @FXML
+    private void fechar_arquivo(ActionEvent event) {
+    }
+
+    @FXML
+    private void abrir_arquivo(ActionEvent event) {
+    }
+
+    @FXML
+    private void aplicar_algoritmo(ActionEvent event) {
+    }
     
 }
--- src/redeneuraismlp/RedeNeuraisMLP.java
+++ src/redeneuraismlp/RedeNeuraisMLP.java
@@ -11,7 +11,7 @@ public class RedeNeuraisMLP extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLTelaPrincipal.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("views/FXMLTelaPrincipal.fxml"));
         
         Scene scene = new Scene(root);
         
